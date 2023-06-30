1860e2c22e11dad5771051b9d5837d060015c69a
betMulat <jbettywor210@gmail.com>
2023-06-30 14:06:30 -0400

    updatejs

--- js/app.js
+++ js/app.js
@@ -1,14 +1,29 @@
 function setup() {
-    createCanvas(800,600);
-    
-}
-function draw() { 
-    background(23,345,56);  
-    fill(168,50,129);
-
-    circle(100, 100, 30);
-    circle(200, 100, 30);
-    noFill();
-    arc(150, 110, 200, 200, 120, 103)
-    
-}
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+        happyFace(width/2, height/2, 170);
+  }
+  
+  function happyFace (x, y, diam) {
+        // Face
+        fill(255, 255, 0);
+        stroke(0);
+        strokeWeight(2);
+        ellipse(x, y, diam, diam);
+        
+        // Smile
+        var startAng = .1*PI
+        var endAng = .9*PI
+        var smileDiam = .6*diam;
+        arc(x, y, smileDiam, smileDiam, startAng, endAng);
+        
+        // Eyes
+        var offset = .2*diam;
+        var eyeDiam = .1*diam;
+        fill(0);
+        ellipse(x-offset, y-offset, eyeDiam, eyeDiam);
+        ellipse(x+offset, y-offset, eyeDiam, eyeDiam);
+  }
