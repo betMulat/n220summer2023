2e4e34f479b0cecef3b48fce3e607d1a5cc20dd2
betMulat <jbettywor210@gmail.com>
2023-06-30 12:23:41 -0400

    First Commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(800,600);
+    
+}
+function draw() { 
+    background(23,345,56);  
+    fill(168,50,129);
+
+    circle(100, 100, 30);
+    circle(200, 100, 30);
+    noFill();
+    arc(150, 110, 200, 200, 120, 103)
+    
+}
