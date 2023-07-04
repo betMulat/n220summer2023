e765f2ac6c7b030f88ee439d6f39cbc3136e1b27
betMulat <jbettywor210@gmail.com>
2023-07-04 00:00:34 -0400

    fixcommit

--- labs/lab2/js/app.js
+++ labs/lab2/js/app.js
@@ -1,5 +1,20 @@
-let spices = 0;
+let xPos = 0;
+let yPos = 0;
+
+let xSpeed = 2;
+let ySpeed = -2;
+function setup(){
+    createCanvas(400, 300);
+}
+
+
 function draw() {
-    spices = spices + 1;
-    console.log(spices);
+    background(194, 178, 134); //clear to black 
+    xPos = xPos + xSpeed;
+    yPos = yPos + ySpeed;
+
+    ySpeed = ySpeed + 0.4;
+    fill(7, 66, 15);
+    circle(yPos, xPos, 25);
+    
 }
