0f085ac7e3c15c460a975505a2a6e98992f6fd60
betMulat <jbettywor210@gmail.com>
2023-07-05 01:03:52 -0400

    final commit

--- labs/lab2Part3/js/app.js
+++ labs/lab2Part3/js/app.js
@@ -1,5 +1,6 @@
-var circleX; //current x position of the circle
-var radius=25; //radius of the circle
+//current x position of the circle and radius of the circle
+var circleX; 
+var radius=25; 
 
 function setup() {
   //creating a 800x600 canvas
@@ -10,12 +11,15 @@ function setup() {
   fill(255,0,0);
 }
 
-
 function draw() {
   //using a gray-ish background
-  background(200);
+  background(117, 50, 168);
+
+
   //drawing a circle at x=circleX, y=height/2 (middle) and with diameter=radius*2
-  circle(circleX, height/2, radius*2);
+  circle(circleX, 300, 50);
+
+  
   //adding 5 to the x value of circle and if it reaches width (800), wrapping
   //around from 0. so that circle will appear on far left when it reaches far right
   circleX=(circleX+5)%width;
