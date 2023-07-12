915d701bda821597999234ed18886f8f39d5ae04
betMulat <jbettywor210@gmail.com>
2023-07-11 20:03:37 -0400

    final commit

--- labs/lab4Part2/js/app.js
+++ labs/lab4Part2/js/app.js
@@ -1,18 +1,24 @@
-var rectXY = []; // start with empty list
+// Starting with empty list of array
+var rectXY = []; 
 function setup() {
   createCanvas(400, 400);
 }
+
 function draw() {
-  background(50);
+//using a purple background 
+  background(235, 52, 198);
+
   noStroke();
   rectMode(CENTER);
   fill(255);
+  //looping and drawing the rectangle 
   for (var i = 0; i < rectXY.length; i++) {
     fill(rectXY[i][2]);
     rect(rectXY[i][0], rectXY[i][1], 50, 25);
     rectXY[i][1] += 1;
   }
 }
+//Adding a new rectangle as the mouse clicked
 function mousePressed() {
   rectXY.push([mouseX, mouseY, random(255)]);
 }
--- labs/lab4Part3/js/app.js
+++ labs/lab4Part3/js/app.js
@@ -1,16 +1,27 @@
 var radius = 20;
+//filling the position x with 11 zeros
 var x = [0,0,0,0,0,0,0,0,0,0,0];
+//filling the position y with 11 zeros
 var y = [0,0,0,0,0,0,0,0,0,0,0];
+
 function setup() {
 createCanvas( 400, 400 );
 }
 
 function draw() {
+//To remove an element from the front of an array we use shift 
 x.shift();
+//To add elements to the end of an Array
 x.push(mouseX);
+//To remove an element from the from of an array we use shift 
 y.shift();
+//To add elements to the end of an Array
 y.push(mouseY);
-background('blue');
+
+//using a purple background 
+background(235, 52, 198);
+
+//looping and drawing all circles at position x and y
 for(let i=0;i<10;i++){
 ellipse( x[i], y[i], radius, radius );
 }
